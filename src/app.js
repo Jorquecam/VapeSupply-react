c9675308a6123dafdab23d8c8915745bb8848fb5
Quesadacambronero <Jordan.Quesadacambronero@nike.com>
2018-05-11 15:29:55 -0600

    Header

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Header from './components/header';
+
+const jsx = (
+    <div>
+        <Header />
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, Image } from 'react-bootstrap';
+
+
+export default class Header extends React.Component{
+     render() {
+         return (
+         <Navbar inverse collapseOnSelect>
+         <Navbar.Header>
+           <Navbar.Brand>
+           <span class="logo">
+                <Image src="..\img\logo.jpg" width="5%" padding=" 7px 14px" rounded />
+           </span>
+           {/* */}
+             <a href="#home">VS Loyalty Points</a>
+           </Navbar.Brand>
+         </Navbar.Header>
+         <Nav>
+           <NavItem eventKey={1}>
+             Link
+           </NavItem>
+           <NavItem eventKey={2}>
+             Link
+           </NavItem>
+           <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+             <MenuItem eventKey={3.1}>Action</MenuItem>
+             <MenuItem eventKey={3.2}>Another action</MenuItem>
+             <MenuItem eventKey={3.3}>Something else here</MenuItem>
+             <MenuItem divider />
+             <MenuItem eventKey={3.4}>Separated link</MenuItem>
+           </NavDropdown>
+         </Nav>
+       </Navbar>);
+     }
+}
